6ef06282ef7e729759bf3cf295713288d305c12e
francobasaglia <francobasaglia94@gmail.com>
2026-08-17 21:42:41 -0300

    chore: improve prepare package json script and related configs

--- scripts/prepare-package-json.js
+++ scripts/prepare-package-json.js
@@ -9,6 +9,21 @@ const [
     packageName
 ] = process.argv.slice(2);
 
+const requiredRootFields = [
+    'version',
+    'description',
+    'repository',
+    'keywords',
+    'author',
+    'license',
+    'bugs',
+    'homepage',
+    'engines'
+];
+const requiredSourceFields = [
+    'name'
+];
+
 if (
     !rootPackageJsonPath ||
     !sourcePackageJsonPath ||
@@ -45,47 +60,113 @@ const transformExports = (value) => {
     return value;
 };
 
-const rootPackage = readJson(rootPackageJsonPath);
-const sourcePackage = readJson(sourcePackageJsonPath);
+const transformBins = (value) => {
+    // @TODO
+    return value;
+};
 
-const outputPackage = {
-    name: sourcePackage.name,
-    version: sourcePackage.version,
-    description: sourcePackage.description ?? rootPackage.description,
-    type: sourcePackage.type,
-    ...(sourcePackage.exports && {
-        exports: transformExports(sourcePackage.exports)
-    }),
-    ...(sourcePackage.dependencies && {
-        dependencies: sourcePackage.dependencies
-    }),
-    ...(rootPackage.repository && {
+const transformDependencies = (dependencies, version) => {
+    return Object.fromEntries(
+        Object.entries(dependencies).map(([dependency, dependencyVersion]) => [
+            dependency,
+            dependency.startsWith('@adalov/')
+                ? version
+                : dependencyVersion
+        ])
+    );
+};
+
+const validateFields = (
+    packageName,
+    rootPackage,
+    sourcePackage
+) => {
+    for (const field of requiredRootFields) {
+        if (rootPackage[field] == null) {
+            throw new Error(`Missing required root package field: ${field}`);
+        }
+    }
+
+    for (const field of requiredSourceFields) {
+        if (sourcePackage[field] == null) {
+            throw new Error(`Missing required source package field: ${field}`);
+        }
+    }
+
+    if (sourcePackage.name !== `@adalov/${packageName}`) {
+        throw new Error(
+            `Package "${packageName}" must be named "@adalov/${packageName}"`
+        );
+    }
+    
+    if (sourcePackage.version !== '0.0.0') {
+        throw new Error(
+            `Source package "${sourcePackage.name}" must use version "0.0.0"`
+        );
+    }
+
+    if (sourcePackage.type !== 'module') {
+        throw new Error(
+            `Source package "${sourcePackage.name}" must use type "module"`
+        );
+    }
+
+    if (sourcePackage.private !== true) {
+        throw new Error(
+            `Source package "${sourcePackage.name}" must be private`
+        );
+    }
+};
+
+const generatePackageJson = (
+    packageName,
+    rootPackageJsonPath,
+    sourcePackageJsonPath,
+    outputPackageJsonPath
+) => {
+    const rootPackage = readJson(rootPackageJsonPath);
+    const sourcePackage = readJson(sourcePackageJsonPath);
+    
+    validateFields(packageName, rootPackage, sourcePackage);
+
+    const outputPackage = {
+        name: sourcePackage.name,
+        version: rootPackage.version,
+        description: sourcePackage.description ?? rootPackage.description,
+        type: 'module',
+        ...(sourcePackage.exports && {
+            exports: transformExports(sourcePackage.exports)
+        }),
+        ...(sourcePackage.bin && {
+            bin: transformBins(sourcePackage.bin)
+        }),
+        ...(sourcePackage.dependencies && {
+            dependencies: transformDependencies(
+                sourcePackage.dependencies,
+                rootPackage.version
+            )
+        }),
         repository: {
             ...rootPackage.repository,
             directory: `packages/${packageName}`
-        }
-    }),
-    ...(rootPackage.keywords && {
-        keywords: rootPackage.keywords
-    }),
-    ...(rootPackage.author && {
-        author: rootPackage.author
-    }),
-    ...(rootPackage.license && {
-        license: rootPackage.license
-    }),
-    ...(rootPackage.bugs && {
-        bugs: rootPackage.bugs
-    }),
-    ...(rootPackage.homepage && {
-        homepage: rootPackage.homepage
-    }),
-    ...(rootPackage.engines && {
+        },
+        keywords: rootPackage.keywords,
+        author: rootPackage.author,
+        license: rootPackage.license,
+        bugs: rootPackage.bugs,
+        homepage: rootPackage.homepage,
         engines: rootPackage.engines
-    })
+    };
+    
+    fs.writeFileSync(
+        outputPackageJsonPath,
+        JSON.stringify(outputPackage, null, 2)
+    );
 };
 
-fs.writeFileSync(
-    outputPackageJsonPath,
-    JSON.stringify(outputPackage, null, 2)
+generatePackageJson(
+    packageName,
+    rootPackageJsonPath,
+    sourcePackageJsonPath,
+    outputPackageJsonPath
 );
