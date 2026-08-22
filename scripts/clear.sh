e8d72364e738d55357450b411b93a3f4befdce9d
Franco Basaglia <francobasaglia94+gpt-bot@gmail.com>
2026-08-22 02:24:30 -0300

    chore(repo): clear package-local test outputs

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -4,12 +4,14 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/shared/packages.sh"
 source "$SCRIPT_DIR/shared/paths.sh"
 
-# Clear Builds outputs
+# Clear package outputs
 for package in "${PACKAGES[@]}"; do
   package_dir="$(PACKAGE_DIR "$package")"
   package_build_dir="$(BUILD_DIR "$package")"
-  
+  package_test_build_dir="$(TEST_BUILD_DIR "$package")"
+
   rm -rf "$package_build_dir"
+  rm -rf "$package_test_build_dir"
   rm -rf "$package_dir/.tsbuildinfo"
 done
 
@@ -17,8 +19,5 @@ done
 rm -rf "$PLAYGROUND_BUILD_DIR"
 rm -rf "$PLAYGROUND_DIR/.tsbuildinfo"
 
-# Clear Unit Test outputs
-rm -rf "$TEST_BUILD_DIR"
-
-# Clear Dist output
+# Clear distribution output
 rm -rf "$DIST_DIR"
